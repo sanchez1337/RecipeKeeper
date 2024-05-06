09cf8b34c4006657cd3231bcd8cbe172c26b096c
Mateusz Iskrzak <mateusz.iskrzak97@gmail.com>
2024-05-06 20:03:29 +0200

    Still issue with getting node_modules linked between container and host

--- frontend/app/.eslintrc.cjs
+++ frontend/app/.eslintrc.cjs
@@ -12,7 +12,8 @@ module.exports = {
     {
       files: [
         'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
+        'cypress/support/**/*.{js,ts,jsx,tsx}',
+        'src/*.{js,ts,jsx,tsx}'
       ],
       'extends': [
         'plugin:cypress/recommended'
